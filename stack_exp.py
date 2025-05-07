431d0372e52cea72548f8bb3d94b7b1f944b083d
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-07 16:30:11 +0530

    Add files via upload

---
+++ stack_exp.py
@@ -0,0 +1,56 @@
+# Stack using list
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.stack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+
+
+print("Top element : ", s.peek())
+print("Stack size : ", s.size())
+
+print("Popped : ", s.pop())
+print("Top after pop : ", s.peek())
+print("Is stack empty?", s.is_empty())
+
+s.pop()
+s.pop()
+print("Is stack empty? ", s.is_empty())
+print("Pop from empty stack : ", s.pop())
+
+
+
+
+
+
+
+
+
+
+
+
+
+

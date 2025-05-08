cad991f652290bb04b30a2d99704e5f1f02b9602
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-08 23:53:21 +0530

    Update tower_of_honoi.py

--- tower_of_honoi.py
+++ tower_of_honoi.py
@@ -11,3 +11,14 @@ def tower_of_honoi(n, source, auxiliary, destination):
 # Example
 n = 3 #number of disk
 tower_of_honoi(n,'A','B', 'C')
+
+
+
+# output 
+# Move disk 1 from A to C
+# Move disk 2 from A to B
+# Move disk 1 from C to B
+# Move disk 3 from A to C
+# Move disk 1 from B to A
+# Move disk 2 from B to C
+# Move disk 1 from A to C

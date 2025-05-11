47a4ca215d45c45b07cff7c43252cb6d24472d25
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-11 16:06:46 +0530

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,15 +1,1 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
 
-def reverse_recursive(head):
-    # Base case: empty list or single node
-    if head is None or head.next is None:
-        return head
-
-    #Recursive call
-    new_head = reverse_recursive(head.next)
-
-    # reverse the current node pointer
-    

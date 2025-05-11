e06da2fe91627e476f695c20cea4d388c47bf715
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-12 00:47:46 +0530

    Update detect_loop_in_linked_list.py

--- detect_loop_in_linked_list.py
+++ detect_loop_in_linked_list.py
@@ -13,4 +13,21 @@ def dectect_loop(head):
             return True
     return False
 
+# Build linked list: 1->2->3->4->5
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+
+#create a loop 5->3
+head.next.next.next.next = head.next.next       #Loop from 5 to 3
+
+#dectect loop
+
+if detect_loop(head):
+    print("Loop detected in linked list")
+else:
+    print("No loop in th linked list")
+
 

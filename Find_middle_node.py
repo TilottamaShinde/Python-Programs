64b12603f3ec224aa242902b97087df134b32c54
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-12 16:40:40 +0530

    Add files via upload

---
+++ Find_middle_node.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+
+        new_node = Node(data)
+        if  not self.head:
+            self.head = new_node
+            return
+
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
+
+    def find_middle(self):
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return  slow.data if slow else None
+
+    def display(self):
+        curr = self.head
+        while curr:
+            print(curr.data, end = ' -> ')
+            curr = curr.next
+        print("None")
+
+# Example usage
+ll = LinkedList()
+for val in [10, 20, 30, 40, 50]:
+    ll.append(val)
+
+ll.display()
+print("Middle Node : ", ll.find_middle())

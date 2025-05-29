92637c6f37236ddd95cc8ffd3013902230fb2029
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-29 23:38:25 +0530

    Update 24_remove_duplicates.py

--- 24_remove_duplicates.py
+++ 24_remove_duplicates.py
@@ -6,3 +6,6 @@ def remove_duplicates(input_list):
 data = [1,2,3,3,4,5,5,1,5]
 unique_data = remove_duplicates(data)
 print("Unique elements are : ", unique_data)
+
+# output
+# Unique elements are :  [1, 2, 3, 4, 5]

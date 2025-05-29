08d4773f316a1e0f1ad3ac4968a2a48f2a462b1e
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-29 23:37:21 +0530

    Add files via upload

---
+++ remove_duplicates.py
@@ -0,0 +1,8 @@
+# program to remove duplicates from list using set
+def remove_duplicates(input_list):
+    return list(set(input_list))
+
+#Example usage
+data = [1,2,3,3,4,5,5,1,5]
+unique_data = remove_duplicates(data)
+print("Unique elements are : ", unique_data)

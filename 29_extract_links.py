6a3f0bf2e3be3ea6404c7fad78730afe0a6ee1ad
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-06-03 23:56:22 +0530

    Update 29_extract_links.py

--- 29_extract_links.py
+++ 29_extract_links.py
@@ -1,6 +1,21 @@
 from bs4 import BeautifulSoup
 
 html_doc = """
-
-
+<html>
+    <body>
+        <h1>Favorite Site</h1>
+        <ul>
+            <li><a href = "https://www.google.com">Google</a></li>
+            <li><a href = "https://www.github.com">GitHub</a></li>
+            <li><a href  = "https://stackoverflow.com">Stack Overflow</a></li>
+        </ul>
+    </body>
+</html>
 """
+soup = BeautifulSoup(html_doc, "html.parser")
+
+#Extract all links and print their text and URLs
+for link in soup.find_all('a'):
+    text = link.get_text()
+    href = link.get('href')
+    print(f"Text: {link.text} | URL: {link['href']}")

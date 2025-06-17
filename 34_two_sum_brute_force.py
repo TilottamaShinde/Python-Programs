307daa91a9b743744772e5224da4e9ff51bc2761
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-06-17 17:38:52 +0530

    Update 34_two_sum_brute_force.py

--- 34_two_sum_brute_force.py
+++ 34_two_sum_brute_force.py
@@ -7,5 +7,12 @@ def two_sum(nums,target):
             if  nums[i] + nums[j] == target:
                 return [i,j]
 
-
+# Example usage
 print(two_sum([2,7,11, 15], 18))
+
+
+'''
+Output:
+[1, 2]
+
+'''
